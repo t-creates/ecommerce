3910f4dc8e15ee7001fbcf830409e9783190878a
Travis Geislinger <travisgeislinger@Traviss-MacBook-Pro.local>
2022-04-28 17:02:15 -0600

    HeroBanner Component

--- ecommerce/components/HeroBanner.jsx
+++ ecommerce/components/HeroBanner.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
 const HeroBanner = () => {
   return (
-    <div>HeroBanner</div>
+    <div className="hero-banner-container">
+      <div>
+        <p className="beats-solo">SMALL TEXT</p>
+        <h3>MID TEXT</h3>
+        <img src="" alt="headphones" className="hero-banner-image" />
+      </div>
+
+      <div>
+        <Link href="./product/ID">
+          <button type="button" >Button Text</button>
+        </Link>
+      </div>
+      <div className="desc">
+        <h5>Description</h5>
+        <p>DESCRIIPTION</p>
+      </div>
+    </div>
   )
 }
 
