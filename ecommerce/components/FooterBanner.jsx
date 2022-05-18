ec2e81cb13d77f06b9e6208ed436e0a6f7066010
Travis Geislinger <travisgeislinger@Traviss-MacBook-Pro.local>
2022-05-18 14:54:22 -0600

    footerBanner component

--- ecommerce/components/FooterBanner.jsx
+++ ecommerce/components/FooterBanner.jsx
@@ -4,9 +4,29 @@ import Link from 'next/link';
 // For images from client 
 import { urlFor } from '../lib/client';
 
-const FooterBanner = ({ footerBanner }) => {
+const FooterBanner = ({ footerBanner: { discount, largeText1, largeText2, midText, saleTime, smallText, product, buttonText, image, desc } }) => {
   return (
-    <div className="footer-banner-container">FooterBanner</div>
+    <div className="footer-banner-container">
+      <div className="banner-desc" >
+        <div className="left">
+          <p>{discount}</p>
+          <h3>{largeText1}</h3>
+          <h3>{largeText2}</h3>
+          <p>{saleTime}</p>
+        </div>
+        <div className="right">
+          <p>{smallText}</p>
+          <h3>{midText}</h3>
+          <p>{desc}</p>
+          <Link href={`/product${product}`}>
+            <button type='button'>{buttonText}</button>
+          </Link>
+        </div>
+        <img
+          src={urlFor(image)} className="footer-banner-image"
+        />
+      </div>
+    </div>
   )
 }
 
